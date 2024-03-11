c1b0d44d44310e525ad07b08433fd6be3e97aec0
Daniel Amaya <49620375+daldev14@users.noreply.github.com>
2024-03-11 10:11:04 -0600

    Add links

--- src/data/links.ts
+++ src/data/links.ts
@@ -23,4 +23,52 @@ export const LINKS = [
     url: "https://tabler.io/icons",
     tags: ["svg", "icons"],
   },
+  {
+    name: "Caninclude",
+    baseUrl: "https://caninclude.glitch.me/",
+    url: "https://caninclude.glitch.me/",
+    tags: ["html", "seo"],
+  },
+  {
+    name: "React Icons",
+    baseUrl: "https://react-icons.github.io/react-icons/",
+    url: "https://react-icons.github.io/react-icons/",
+    tags: ["svg", "react"],
+  },
+  {
+    name: "JSON Crack",
+    baseUrl: "https://jsoncrack.com/",
+    url: "https://jsoncrack.com/",
+    tags: ["json", "visualizing"],
+  },
+  {
+    name: "Operator Lookup",
+    baseUrl: "https://www.joshwcomeau.com/",
+    url: "https://www.joshwcomeau.com/operator-lookup/",
+    tags: ["javascript", "operators"],
+  },
+  {
+    name: "Bundlephobia",
+    baseUrl: "https://bundlephobia.com/",
+    url: "https://bundlephobia.com/",
+    tags: ["npm", "dependencies"],
+  },
+  {
+    name: "JS Benchmarking - Husty",
+    baseUrl: "https://hasty.dev/",
+    url: "https://hasty.dev/new",
+    tags: ["javascript", "benchmarking", "performance", "optimization"],
+  },
+  {
+    name: "Unminify",
+    baseUrl: "https://unminify.com/",
+    url: "https://unminify.com/",
+    tags: ["javascript", "unminify", "css", "html", "xml", "json"],
+  },
+  {
+    name: "RegExr",
+    baseUrl: "https://regexr.com/",
+    url: "https://regexr.com/",
+    tags: ["regex", "regular expression"],
+  },
 ];
